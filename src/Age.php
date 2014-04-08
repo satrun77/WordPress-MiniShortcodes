611c3b438d0d802618735b08b6954dfd37e6367f
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2014-04-08 21:23:25 +1200

    Bug fixes and misc changes

--- src/Age.php
+++ src/Age.php
@@ -3,14 +3,14 @@
 defined('MOO_MINSHORTCODE') or die;
 
 /**
- * A shortcode to display age based on a date
+ * A shortcode to display age as string based on a date
  * Usage:
- *  [moo_age date="1982-1-13" append=" years old" debug="true"]
+ *  [moo_age date="1982-1-13" append=" years old"]
  *
  * @copyright  2012 Mohamed Alsharaf
  * @author     Mohamed Alsharaf (mohamed.alsharaf@gmail.com)
  * @version    1.0.0
- * @license    http://opensource.org/licenses/MIT
+ * @license    The MIT License (MIT)
  */
 class Moo_MiniShortcodes_Age implements Moo_ShortcodeInterface
 {
@@ -37,7 +37,7 @@ class Moo_MiniShortcodes_Age implements Moo_ShortcodeInterface
             throw new Exception(sprintf("You are an unborn child! [Your age is %s]", $diff));
         }
 
-        $append = isset($atts['append']) ? $atts['append'] : '';
+        $append = isset($atts['append']) ? $atts['append'] : ' years old';
         return $diff . $append;
     }
 
